7ceb7a48674c3515550e95e858a981d6f27bbc7b
Ivo Perpina <iperpina@bnasdev.onmicrosoft.com>
2019-08-05 11:49:37 -0300

    Unit test switch to Jest

--- src/components/shared/grid/Grid.test.js
+++ src/components/shared/grid/Grid.test.js
@@ -1,45 +1,42 @@
-import { describe } from 'riteway';
-import render from 'riteway/render-component';
 import React from 'react';
 import GnomesApi from '../../../apis/apis.gnomes';
 import Grid from './Grid';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-describe('Grid Component', async assert => {
+Enzyme.configure({ adapter: new Adapter() });
+
+test("Grid component", () => {
     // Arrange
-        const state = {
-            items: [
-                {
-                  'id':0,
-                  'name':'Tobus Quickwhistle',
-                  'thumbnail':'',
-                  'age':306,
-                  'weight':39.065952,
-                  'height':107.75835,
-                  'hair_color':'Pink',
-                  'professions':['Metalworker','Woodcarver','Stonecarver',' Tinker','Tailor','Potter'],
-                  'friends':[]
-                },
-                {
-                  'id':1,
-                  'name':'Fizkin Voidbuster',
-                  'thumbnail':'',
-                  'age':288,
-                  'weight':35.279167,
-                  'height':110.43628,
-                  'hair_color':'Green',
-                  'professions':['Brewer','Medic','Prospector','Gemcutter','Mason','Tailor'],
-                  'friends':[]
-                }],
-            columns: GnomesApi.getGridColumns(),
-        };
+    const state = {
+        items: [
+            {
+              'id':0,
+              'name':'Tobus Quickwhistle',
+              'thumbnail':'',
+              'age':306,
+              'weight':39.065952,
+              'height':107.75835,
+              'hair_color':'Pink',
+              'professions':['Metalworker','Woodcarver','Stonecarver',' Tinker','Tailor','Potter'],
+              'friends':[]
+            },
+            {
+              'id':1,
+              'name':'Fizkin Voidbuster',
+              'thumbnail':'',
+              'age':288,
+              'weight':35.279167,
+              'height':110.43628,
+              'hair_color':'Green',
+              'professions':['Brewer','Medic','Prospector','Gemcutter','Mason','Tailor'],
+              'friends':[]
+            }],
+        columns: GnomesApi.getGridColumns(),
+    };
 
     // Act
-        const $ = render(<Grid items={state.items} columns={state.columns} />);
-    // Assert
-        assert({
-            given: 'an API result',
-            should: 'Render a grid with data',
-            actual: $('.table').find('tr').length,
-            expected: 3,
-        });
+    const wrapper = shallow(<Grid items={state.items} columns={state.columns} />);
+
+    expect(wrapper.find('tr')).toHaveLength(3); //one for headers and two for table results
 });
