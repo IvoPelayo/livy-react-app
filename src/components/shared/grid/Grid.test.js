ed0bf5ca1f674f31c3904d296301f50043efbde9
Ivo Perpina <iperpina@bnasdev.onmicrosoft.com>
2019-08-05 11:51:33 -0300

    columns test

--- src/components/shared/grid/Grid.test.js
+++ src/components/shared/grid/Grid.test.js
@@ -6,37 +6,47 @@ import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test("Grid component", () => {
+const state = {
+    items: [
+        {
+          'id':0,
+          'name':'Tobus Quickwhistle',
+          'thumbnail':'',
+          'age':306,
+          'weight':39.065952,
+          'height':107.75835,
+          'hair_color':'Pink',
+          'professions':['Metalworker','Woodcarver','Stonecarver',' Tinker','Tailor','Potter'],
+          'friends':[]
+        },
+        {
+          'id':1,
+          'name':'Fizkin Voidbuster',
+          'thumbnail':'',
+          'age':288,
+          'weight':35.279167,
+          'height':110.43628,
+          'hair_color':'Green',
+          'professions':['Brewer','Medic','Prospector','Gemcutter','Mason','Tailor'],
+          'friends':[]
+        }],
+    columns: GnomesApi.getGridColumns(),
+};
+
+test("Grid component should render items", () => {
     // Arrange
-    const state = {
-        items: [
-            {
-              'id':0,
-              'name':'Tobus Quickwhistle',
-              'thumbnail':'',
-              'age':306,
-              'weight':39.065952,
-              'height':107.75835,
-              'hair_color':'Pink',
-              'professions':['Metalworker','Woodcarver','Stonecarver',' Tinker','Tailor','Potter'],
-              'friends':[]
-            },
-            {
-              'id':1,
-              'name':'Fizkin Voidbuster',
-              'thumbnail':'',
-              'age':288,
-              'weight':35.279167,
-              'height':110.43628,
-              'hair_color':'Green',
-              'professions':['Brewer','Medic','Prospector','Gemcutter','Mason','Tailor'],
-              'friends':[]
-            }],
-        columns: GnomesApi.getGridColumns(),
-    };
 
     // Act
     const wrapper = shallow(<Grid items={state.items} columns={state.columns} />);
 
     expect(wrapper.find('tr')).toHaveLength(3); //one for headers and two for table results
+});
+
+test("Grid component should have the right amount of columns", () => {
+    // Arrange
+
+    // Act
+    const wrapper = shallow(<Grid items={state.items} columns={state.columns} />);
+
+    expect(wrapper.find('th')).toHaveLength(5); //columns are set on api.gnomes.js
 });
